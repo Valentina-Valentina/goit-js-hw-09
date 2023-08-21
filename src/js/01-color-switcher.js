402782eec51fa5f8ad0369b60ad5c71f5ecad1dd
Valentina <valentinasergssvna@gmail.com>
2023-08-21 21:25:06 +0300

    add js-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
 
+
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+let timerId = null;
+startBtn.addEventListener("click", onClickStartBtn);
+stopBtn.addEventListener("click", onClickStopBtn);
+
+function onClickStartBtn() {
+    timerId = setInterval(() => {
+        startBtn.disabled = true; 
+        body.style.backgroundColor = getRandomHexColor()   
+    }, 1000)
+      
+}
+
+function onClickStopBtn() {
+    startBtn.disabled = false;
+    clearInterval(timerId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
